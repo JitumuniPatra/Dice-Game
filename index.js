a241ab7a4d2b86c72ae6f9aa1fe865b701dde63f
Jitumuni Patra <57381217+JitumuniPatra@users.noreply.github.com>
2020-01-31 00:04:17 +1000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+var randomNum1 = Math.random() * 6 + 1;
+randomNum1 = Math.floor(randomNum1);
+console.log("Num1 : " + randomNum1);
+
+
+var randomNum2 = Math.random() * 6 + 1;
+randomNum2 = Math.floor(randomNum2);
+console.log("Num2 : " + randomNum2);
+
+//change img number 1.
+var imgName1 = "images/dice" + randomNum1 + ".png";
+var element = document.getElementsByClassName('img1');
+element[0].src = imgName1;
+
+//change img number 2.
+
+var imgName2 = "images/dice" + randomNum2 + ".png";
+var element = document.getElementsByClassName('img2');
+element[0].src = imgName2;
+
+//check who win//
+var headElement = document.querySelector("h1");
+if(randomNum1 > randomNum2){
+  headElement.innerHTML="🚩 Player 1 win";
+}
+else if(randomNum1<randomNum2){
+  headElement.innerHTML="Player2 Win 🚩";}
+
+else{
+  headElement.innerHTML="Match Draw !";}
